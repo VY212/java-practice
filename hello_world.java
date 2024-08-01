940e44c4d7b2a329a5bcaa704bb649999713ea24
Vivek Yadav <vy8529200146@gmail.com>
2024-08-01 13:50:47 +0530

    this is the first program in java program

---
+++ hello_world.java
@@ -0,0 +1,11 @@
+package com.vivek;
+
+import java.util.Scanner;
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
